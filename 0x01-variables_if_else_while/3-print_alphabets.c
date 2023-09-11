99768659a65933538eb14983a07cbbeece7b1c55
Lemmy <osikilemmy@gmail.com>
2023-09-11 12:39:32 -0700

    made change to prit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main(void)
+  /**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 {
   char lowercase = 'a';
   char uppercase = 'A';
@@ -14,5 +19,5 @@ int main(void)
       uppercase++;
     }
   putchar('\n');
-  return(0);
+  return (0);
 }
