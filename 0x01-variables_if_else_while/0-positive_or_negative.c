1d99667b9b04893d793253b8e5e1077550f25593
Lemmy <osikilemmy@gmail.com>
2023-09-11 11:15:20 -0700

    made change to postiv

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h> /* Include stdio.h for printf */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
