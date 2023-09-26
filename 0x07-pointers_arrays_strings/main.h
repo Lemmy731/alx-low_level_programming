5afaa047f8626f7a6068fc348b96466b6a353d05
Lemmy <osikilemmy@gmail.com>
2023-09-26 14:20:50 -0700

    made change to chess

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototype */
-char *_strstr(char *haystack, char *needle);
+/* Function prototype for _putchar */
+int _putchar(char c);
+
+/* Function prototype for print_chessboard */
+void print_chessboard(char (*a)[8]);
+
 #endif /* MAIN_H */
