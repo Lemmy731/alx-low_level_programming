4d54777dbfcc9b714fcc448be169f0cce710926f
Lemmy <osikilemmy@gmail.com>
2023-09-26 14:58:43 -0700

    made change to str

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototype for _putchar */
-int _putchar(char c);
-
-/* Function prototype for print_diagsums */
-void print_diagsums(int *a, int size);
+/* Function prototypes */
+char *_strstr(char *haystack, char *needle);
 
 #endif /* MAIN_H */
