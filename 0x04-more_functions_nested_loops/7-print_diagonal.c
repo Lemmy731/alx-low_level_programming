6a03484b16bb6ca82ecf1c98e1c0abb56d525de0
Lemmy731 <osikilemmy@gmail.com>
2023-10-13 12:49:22 -0700

    made change to 7print

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,13 +6,13 @@
  */
 void print_diagonal(int n)
 {
+	int i, j;
 	if (n <= 0)
 	{
 	_putchar('\n');
 	}
 	else
 	{
-	int i, j;
 	for (i = 0; i < n; i++)
 	{
 	for (j = 0; j < i; j++)
@@ -20,7 +20,7 @@ void print_diagonal(int n)
 	_putchar(32);
 	}
 	_putchar(92);
-	_putchar('\n')
+	_putchar('\n');
 	}
 	}
 }
