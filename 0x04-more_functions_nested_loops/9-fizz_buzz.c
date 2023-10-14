a8ca4961955dcf300a539120a2b3563f5b5abf27
Lemmy731 <osikilemmy@gmail.com>
2023-10-13 21:01:49 -0700

    made change to 9fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,27 +5,39 @@
  */
 void fizz_buzz(void)
 {
-    int i;
+	int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0)
-        {
-            _puts("Fizz");
-        }
-        if (i % 5 == 0)
-        {
-            _puts("Buzz");
-        }
-        if (i % 3 != 0 && i % 5 != 0)
-        {
-            _putnum(i);
-        }
-        if (i != 100)
-        {
-            _putchar(' ');
-        }
-    }
-    _putchar('\n');
+	for (i = 1; i <= 100; i++)
+	{
+	if (i % 3 == 0)
+	{
+	_putchar('F');
+	_putchar('i');
+	_putchar('z');
+	_putchar('z');
+	}
+	if (i % 5 == 0)
+	{
+	_putchar('B');
+	_putchar('u');
+	_putchar('z');
+	_putchar('z');
+	}
+	if (i % 3 != 0 && i % 5 != 0)
+	{
+	int num = i;
+	if (num >= 10) {
+	_putchar((num / 10) + '0');
+	num %= 10;
+	}
+	_putchar(num + '0');
+	}
+	if (i != 100)
+	{
+	_putchar(' ');
+	}
+	}
+	_putchar('\n');
 }
+
 
