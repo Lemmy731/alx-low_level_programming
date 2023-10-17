7eaf4f4b5fb07455fa954e1756206edd7254196c
Lemmy731 <osikilemmy@gmail.com>
2023-10-17 13:16:08 -0700

    made change to 9str

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,16 +10,16 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-    char *dest_start = dest;
+char *dest_start = dest;
 
-    while (*src)
-    {
-        *dest = *src;
-        dest++;
-        src++;
-    }
+while (*src)
+{
+*dest = *src;
+dest++;
+src++;
+}
 
-    *dest = '\0';
+*dest = '\0';
 
-    return (dest_start);
+return (dest_start);
 }
