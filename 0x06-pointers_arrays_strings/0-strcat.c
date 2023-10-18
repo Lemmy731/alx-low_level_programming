34df50a926d12081369e52f4024cf4a49d760ec9
Lemmy731 <osikilemmy@gmail.com>
2023-10-18 12:58:25 -0700

    made change to 0str

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,40 +2,31 @@
 
 /**
  * _strcat - Concatenates two strings.
- * @s1: The first string.
- * @s2: The second string.
+ * @dest: The destination string to which src will be appended.
+ * @src: The source string to append to dest.
  *
- * Return: A pointer to the concatenated string.
+ * Return: A pointer to the resulting string (dest).
  */
-char *_strcat(char *s1, char *s2)
+char *_strcat(char *dest, char *src)
 {
-int len1 = 0, len2 = 0, i, j;
-char *concatenated;
+char *result = dest; // Store the starting address of dest
 
-    /* Calculate the length of s1 */
-while (s1[len1])
-len1++;
-
-    /* Calculate the length of s2 */
-while (s2[len2])
-len2++;
-
-    /* Allocate memory for the concatenated string */
-concatenated = malloc((len1 + len2 + 1) * sizeof(char));
-
-if (concatenated == NULL)
-return (NULL);
-
-    /* Copy the characters from s1 to the new string */
-for (i = 0; i < len1; i++)
-concatenated[i] = s1[i];
-
-    /* Copy the characters from s2 to the new string */
-for (j = 0; j < len2; j++)
-concatenated[i + j] = s2[j];
-
-    /* Null-terminate the concatenated string */
-concatenated[i + j] = '\0';
-
-return (concatenated);
+   
+while (*dest)
+{
+dest++;
 }
+
+   
+while (*src)
+{
+*dest = *src;
+dest++;
+src++;
+}
+
+*dest = '\0'; 
+
+return result;
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);
 char *_strncpy(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
 
 #endif /* MAIN_H */
