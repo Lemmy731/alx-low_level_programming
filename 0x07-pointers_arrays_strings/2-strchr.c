3c495cc53d6923d44c244468eecf7deec1235915
Lemmy731 <osikilemmy@gmail.com>
2023-10-23 13:16:07 -0700

    made change to 2str

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * _strchr - Locates a character in a string.
- * @s: Pointer to the string.
- * @c: The character to locate.
- * Return: Pointer to the first occurrence of the character c in s, or NULL if not found.
+ * _strchr - locates a character in a string
+ * @s: the string to search
+ * @c: the character to find
+ *
+ * Return: pointer to the first occurrence of the character c in s, or NULL if not found
  */
 char *_strchr(char *s, char c)
 {
-while (*s != '\0')
+while (*s)
 {
 if (*s == c)
-{
 return s;
-}
 s++;
 }
-
-
+if (*s == c)
+return s;
 return NULL;
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 
 /* Function prototypes */
 char  _strstr(char *haystack, char *needle);
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */
