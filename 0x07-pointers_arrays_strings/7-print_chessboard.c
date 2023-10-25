c5d9fdca126c85d6c17f8ba2b7600b374de34008
Lemmy731 <osikilemmy@gmail.com>
2023-10-24 21:22:29 -0700

    made change to 7print

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_chessboard - Prints a chessboard.
- * @a: 2D array representing the chessboard.
+ * @s: Pointer to the string to search.
+ * @accept: Pointer to the string containing acceptable bytes.
+ * Return: Pointer to the byte in s that matches one of the bytes in accept, or NULL if not found.
  */
 void print_chessboard(char (*a)[8])
 {
-int row, col;
+int row; 
+int col;
 
 for (row = 0; row < 8; row++)
 {
 for (col = 0; col < 8; col++)
-{
 _putchar(a[row][col]);
-}
 _putchar('\n');
 }
 }
