d7b7f9818bf028e1b628e0d475b9f95e6ed0b40e
Lemmy731 <osikilemmy@gmail.com>
2023-10-26 12:46:16 -0700

    made change to 5sqrt

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+6#ifndef MAIN_H
 #define MAIN_H
 
 /* Provided putchar function */
@@ -10,5 +10,6 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
 
 #endif /* MAIN_H */
