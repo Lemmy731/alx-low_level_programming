43f8d0b3ebae9ca6b31797d8552c79bc9a3b5738
Lemmy731 <osikilemmy@gmail.com>
2023-10-26 20:38:57 -0700

    made change to 6prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,12 +10,12 @@
 int is_divisible(int n, int divisor)
 {
 	if (divisor == 1)
-	return 1;
+	return (1);
 
 	if (n % divisor == 0)
-	return 0;
+	return (0);
 
-	return is_divisible(n, divisor - 1);
+	return (is_divisible(n, divisor - 1));
 }
 
 /**
@@ -27,7 +27,7 @@ int is_divisible(int n, int divisor)
 int is_prime_number(int n)
 {
 	if (n <= 1)
-	return 0;
+	return (0);
 
-	return is_divisible(n, n - 1);
+	return (is_divisible(n, n - 1));
 }
