30b2e3043e83ec8bac3d17a7c8caa621a35e207f
Lemmy731 <osikilemmy@gmail.com>
2023-10-26 19:59:13 -0700

    made change to 5sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,20 +7,6 @@
  *
  * Return: The square root of n, or -1 if it doesn't have a natural square root
  */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-	return -1;
-
-	return find_sqrt(n, 0);
-}
-
-/**
- * _sqrt_recursion - Calculate the natural square root of a number
- * @n: The number for which to find the square root
- *
- * Return: The square root of n, or -1 if it doesn't have a natural square root
- */
 
 int find_sqrt(int n, int guess)
 {
@@ -31,4 +17,20 @@ int find_sqrt(int n, int guess)
 	return -1;
 
 	return find_sqrt(n, guess + 1);
+}
+
+
+/**
+ * _sqrt_recursion - Calculate the natural square root of a number
+ * @n: The number for which to find the square root
+ *
+ * Return: The square root of n, or -1 if it doesn't have a natural square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	return -1;
+
+	return find_sqrt(n, 0);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,4 @@
-6#ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 /* Provided putchar function */
