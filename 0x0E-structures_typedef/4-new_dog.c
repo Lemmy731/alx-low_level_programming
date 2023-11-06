8ea1268e70ead984933e9ee8a6195ebbfeb16e30
Lemmy731 <osikilemmy@gmail.com>
2023-11-06 15:33:01 -0800

    made change to 4new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,3 +1,31 @@
+#include <stdlib.h>
+#include "dog.h"
+
+
+/**
+* _strlen - returns the length of a string
+* @s: string to evaluate
+*
+* Return: the length of the string
+*/
+	int _strlen(char *s)
+	{
+		int i;
+
+
+		i = 0;
+
+
+		while (s[i] != '\0')
+		{
+			i++;
+		}
+
+
+		return (i);
+	}
+
+
 /**
 * *_strcpy - copies the string pointed to by src
 * including the terminating null byte (\0)
@@ -31,8 +59,9 @@
 		return (dest);
 	}
 
+
 /**
-* new_dog - creates a new do
+* new_dog - creates a new dog
 * @name: name of the dog
 * @age: age of the dog
 * @owner: owner of the dog
@@ -74,4 +103,3 @@
 
 		return (dog);
 	}
-
